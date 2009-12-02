eaf4d47cb3f483e231d55040062306442596209c
Philip Arndt <parndt@gmail.com>
2009-12-03 12:36:46 +1300

    update version

--- rails/init.rb
+++ rails/init.rb
@@ -3,13 +3,13 @@ Refinery::Plugin.register do |plugin|
 	plugin.title = "Portfolio"
 	plugin.description = "Manage a portfolio"
 	plugin.url = "/admin/#{plugin.title.downcase}"
-	plugin.version = '0.9.1.5'
+	plugin.version = '0.9.1.8'
 	plugin.menu_match = /admin\/((portfolio)|(portfolio_entries))/
 	plugin.activity = {
 		:class => PortfolioEntry,
-		:title => 'title', 
-		:url_prefix => 'edit', 
-		:created_image => "layout_add.png", 
+		:title => 'title',
+		:url_prefix => 'edit',
+		:created_image => "layout_add.png",
 		:updated_image => "layout_edit.png"
 	}
 end
