b6a13aff9f3a05c1441bb75bfc6cbfaa7ab3e06e
Philip Arndt <parndt@gmail.com>
2010-08-23 21:41:12 +1200

    Fixes seeds for Portfolio vs portfolio.

--- lib/generators/portfolio/templates/db/seeds/portfolio.rb
+++ lib/generators/portfolio/templates/db/seeds/portfolio.rb
@@ -1,8 +1,14 @@
 User.find(:all).each do |user|
-  user.plugins.create(:name => "Portfolio", :position => (user.plugins.maximum(:position) || -1) +1)
+  user.plugins.create(:name => "portfolio", :position => (user.plugins.maximum(:position) || -1) +1)
 end
 
-page = Page.create(:title => "Portfolio", :link_url => "/portfolio", :menu_match => "\/portfolio(|\/.+?)", :deletable => false, :position => ((Page.maximum(:position, :conditions => {:parent_id => nil}) || -1)+1))
+page = Page.create({
+  :title => "Portfolio",
+  :link_url => "/portfolio",
+  :menu_match => "\/portfolio(|\/.+?)",
+  :deletable => false,
+  :position => ((Page.maximum(:position, :conditions => {:parent_id => nil}) || -1)+1)
+})
 RefinerySetting.find_or_set(:default_page_parts, ["Body", "Side Body"]).each do |default_page_part|
   page.parts.create(:title => default_page_part, :body => nil)
 end
