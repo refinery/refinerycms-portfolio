741992b89ba73a2d81ff23fe238a8be19abbc29f
Philip Arndt <parndt@gmail.com>
2010-09-27 14:57:48 +1300

    has_many :through allows us to order properly.

--- app/models/portfolio_entry.rb
+++ app/models/portfolio_entry.rb
@@ -7,14 +7,17 @@ class PortfolioEntry < ActiveRecord::Base
   acts_as_nested_set
   default_scope :order => 'lft ASC'
 
-  has_and_belongs_to_many :images
-  accepts_nested_attributes_for :images
+  has_many :images_portfolio_entries
+  has_many :images, :through => :images_portfolio_entries, :order => 'images_portfolio_entries.position ASC'
+  accepts_nested_attributes_for :images, :allow_destroy => false
 
   def images_attributes=(data)
     self.images.clear
-
-    self.images += (0..(data.length-1)).collect { |i|
-      (Image.find(data[i.to_s]['id'].to_i) rescue nil)
+    
+    self.images = (0..(data.length-1)).collect { |i|
+      unless (image_id = data[i.to_s]['id'].to_i) == 0
+        Image.find(image_id) rescue nil
+      end
     }.compact
   end
 
