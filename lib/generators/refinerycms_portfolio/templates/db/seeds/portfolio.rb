d95f33ebb71f616f64143f3f515d7ec2bb10c5ee
Philip Arndt <parndt@gmail.com>
2010-11-01 10:15:12 +1300

    We don't use settings for image sizes anymore.

--- lib/generators/refinerycms_portfolio/templates/db/seeds/portfolio.rb
+++ lib/generators/refinerycms_portfolio/templates/db/seeds/portfolio.rb
@@ -15,16 +15,3 @@ page = Page.create({
 Page.default_parts.each do |default_page_part|
   page.parts.create(:title => default_page_part, :body => nil)
 end
-
-# we need to retrieve the value, merge it in and then save it back because it's a frozen hash.
-image_thumbnails = RefinerySetting.find_or_set(:image_thumbnails, {}).dup
-new_thumbnails = image_thumbnails.merge({
-  :portfolio_thumb => '96x96#c',
-  :portfolio => '600x512'
-})
-# handles a change in Refinery API
-if RefinerySetting.methods.map(&:to_sym).include?(:set)
-  RefinerySetting.set(:image_thumbnails, new_thumbnails)
-else
-  RefinerySetting[:image_thumbnails] = new_thumbnails
-end
