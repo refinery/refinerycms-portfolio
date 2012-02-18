fa5eea98022df034a1a4a4e85cb614c5fba7c3fe
Philip Arndt <parndt@gmail.com>
2012-02-18 15:17:08 +1300

    Use Refinery::Core::BaseModel

--- app/models/refinery/portfolio/gallery.rb
+++ app/models/refinery/portfolio/gallery.rb
@@ -1,7 +1,7 @@
 module Refinery
   module Portfolio
-    class Gallery < Refinery::Core::Base
-      
+    class Gallery < Refinery::Core::BaseModel
+
       acts_as_indexed :fields => [:title, :body]
       validates :title, :presence => true, :uniqueness => true
       attr_accessible :title, :body, :parent_id, :lft, :rgt, :position, :gallery_type, :depth
@@ -9,15 +9,14 @@ module Refinery
       alias_attribute :description, :body
 
       acts_as_nested_set :dependent => :destroy
-      has_friendly_id :title, 
+      has_friendly_id :title,
         :use_slug => true,
         :default_locale => (::Refinery::I18n.default_frontend_locale rescue :en),
         :approximate_ascii => ::Refinery::Portfolio.approximate_ascii,
         :strip_non_ascii => ::Refinery::Portfolio.strip_non_ascii
 
-      has_many :items, :class_name => 'Refinery::Portfolio::Item'
+      has_many :items
 
-      
     end
   end
 end
