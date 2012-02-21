9ea54c77e5e4d4d6b8da0d903f2fe5ae51f27aad
Rob Yurkowski <rob@godynamo.com>
2012-02-20 23:47:02 -0500

    Reorganize gallery model, introduce scoping to its friendly_id, and fix a translation issue on its admin form.

--- app/models/refinery/portfolio/gallery.rb
+++ app/models/refinery/portfolio/gallery.rb
@@ -1,22 +1,21 @@
 module Refinery
   module Portfolio
     class Gallery < Refinery::Core::BaseModel
-
       acts_as_indexed :fields => [:title, :body]
-      validates :title, :presence => true, :uniqueness => true
-      attr_accessible :title, :body, :parent_id, :lft, :rgt, :position, :gallery_type, :depth
-
-      alias_attribute :description, :body
-
       acts_as_nested_set :dependent => :destroy
       has_friendly_id :title,
         :use_slug => true,
         :default_locale => (::Refinery::I18n.default_frontend_locale rescue :en),
         :approximate_ascii => ::Refinery::Portfolio.approximate_ascii,
-        :strip_non_ascii => ::Refinery::Portfolio.strip_non_ascii
+        :strip_non_ascii => ::Refinery::Portfolio.strip_non_ascii,
+        :scope => :parent
 
       has_many :items
 
+      attr_accessible :title, :body, :parent_id, :lft, :rgt, :position, :gallery_type, :depth
+      alias_attribute :description, :body
+
+      validates :title, :presence => true, :uniqueness => true
     end
   end
 end
