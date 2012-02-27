8de7b7d270e77224219f48b60647ece043408361
Uģis Ozols <ugis.ozolss@gmail.com>
2012-02-27 17:15:21 +0200

    Remove Refinery::Page.load_seed from seeds file and remove position column.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,20 @@
 if defined?(::Refinery::User)
   ::Refinery::User.all.each do |user|
     if user.plugins.where(:name => 'refinerycms-portfolio').blank?
-      user.plugins.create(:name => 'refinerycms-portfolio',
-                          :position => (user.plugins.maximum(:position) || -1) +1)
+      user.plugins.create(:name => 'refinerycms-portfolio')
     end
   end
 end
-
 
 url = "/portfolio"
 if defined?(::Refinery::Page) && ::Refinery::Page.where(:link_url => url).empty?
   page = ::Refinery::Page.create(
     :title => 'Portfolio',
     :link_url => url,
-    :deletable => false,
-    :position => ((::Refinery::Page.maximum(:position, :conditions => {:parent_id => nil}) || -1)+1),
+    :deletable => false
     :menu_match => "^#{url}(\/|\/.+?|)$"
   )
   Refinery::Pages.default_parts.each do |default_page_part|
     page.parts.create(:title => default_page_part, :body => nil)
   end
 end
-
-
-# Added by RefineryCMS Pages engine
-Refinery::Pages::Engine.load_seed
