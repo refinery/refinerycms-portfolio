867a14552908d73e36e9ea6fb4067ed9d99a2728
Rob Yurkowski <rob@godynamo.com>
2012-03-05 00:36:41 -0500

    General enhancements, including showing image preview in backend, adding front-end styles for galleries.

--- lib/refinery/portfolio/configuration.rb
+++ lib/refinery/portfolio/configuration.rb
@@ -5,6 +5,6 @@ module Refinery
     config_accessor :items_per_page, :cover_image_thumb_size
 
     self.items_per_page = 20
-    self.cover_image_thumb_size = '200x200'
+    self.cover_image_thumb_size = '300x200'
   end
 end
