f95cf7eb6a38bbee3f3049f5e16c74833142dbca
Rob Yurkowski <rob@godynamo.com>
2012-03-05 02:23:35 -0500

    Fixed stylesheet typo, added link to skitter doc, expanded options for skitter.

--- lib/refinery/portfolio/configuration.rb
+++ lib/refinery/portfolio/configuration.rb
@@ -8,7 +8,15 @@ module Refinery
     self.cover_image_thumb_size = '300x200'
 
     self.skitter_options = {
-      :interval => 5000
+      :interval => 5000,            # Duration of slides in ms
+      :animation => :circles,       # Animation to use
+      :numbers => true,             # Use number-based nav
+      :dots => false,               # Use dot-based nav
+      :navigation => true,          # Show navigation
+      :hideTools => false,          # Hide tools (?)
+      :fullscreen => false,         # Use fullscreen (no support, caveat spector)
+      :numbers_align => :center     # Nav placement (left, right, center)
+      # :structure => ""            # Use this to override the HTML if utterly necessary.
     }
   end
 end
