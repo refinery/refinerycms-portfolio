17985cb1792782bfbe16ca362b20a83e0e970472
Rob Yurkowski <rob@godynamo.com>
2012-03-05 02:00:40 -0500

    Adds skitter. Wow.

--- lib/refinery/portfolio/configuration.rb
+++ lib/refinery/portfolio/configuration.rb
@@ -2,9 +2,13 @@ module Refinery
   module Portfolio
     include ActiveSupport::Configurable
 
-    config_accessor :items_per_page, :cover_image_thumb_size
+    config_accessor :items_per_page, :cover_image_thumb_size, :skitter_options
 
     self.items_per_page = 20
     self.cover_image_thumb_size = '300x200'
+
+    self.skitter_options = {
+      :interval => 5000
+    }
   end
 end
