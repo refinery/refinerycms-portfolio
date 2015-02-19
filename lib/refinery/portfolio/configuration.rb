0ea9edc1c2b7b892e16001ca8968d9ac607feb6e
Brice Sanchez <brice@bisscomm.com>
2015-02-19 11:00:32 -0500

    Change hardcoded page-url for a config

--- lib/refinery/portfolio/configuration.rb
+++ lib/refinery/portfolio/configuration.rb
@@ -3,12 +3,15 @@ module Refinery
     include ActiveSupport::Configurable
 
     config_accessor :items_per_page, :cover_image_thumb_size,
-                    :display_item_text, :display_gallery_text
+                    :display_item_text, :display_gallery_text,
+                    :page_url
 
     self.items_per_page = 20
     self.cover_image_thumb_size = '300x200'
 
     self.display_item_text = true
     self.display_gallery_text = true
+
+    self.page_url = '/portfolio'
   end
 end
